af57349cea247429adf343f70f24e178833af5a3
Romaric Zounlome <rzounlom@gmail.com>
2024-04-02 18:51:09 -0400

    update blog app styles

--- app-example/my-blog/src/data/posts.js
+++ app-example/my-blog/src/data/posts.js
@@ -119,4 +119,22 @@ export const featuredPosts = [
       "https://images.unsplash.com/photo-1541464522988-31b420f688b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
     text: "I'm baby tumeric XOXO photo booth actually, helvetica semiotics austin copper mug. Ascot YOLO crucifix biodiesel DSA occupy. Prism live-edge iPhone semiotics you probably haven't heard of them seitan. Irony shaman 3 wolf moon tacos actually marfa, ascot vegan. Meggings fanny pack before they sold out slow-carb synth literally. Godard chicharrones mustache vegan occupy mumblecore craft beer hella yuccie chartreuse kombucha. Fam bespoke cloud bread actually, stumptown raw denim mukbang hashtag pok pok polaroid banjo master cleanse sartorial. Cray vegan tilde dreamcatcher actually cliche. Waistcoat venmo YOLO, pok pok raclette kombucha locavore williamsburg. Polaroid jianbing yes plz godard sartorial post-ironic shoreditch health goth pinterest. Ugh iceland keffiyeh retro narwhal tumblr",
   },
+  {
+    id: "5",
+    publishedDate: "05-11-2023",
+    title: "Utah is dope!",
+    author: "Sarah Parker",
+    imgUrl:
+      "https://plus.unsplash.com/premium_photo-1675824612037-72045baa2109?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    text: "I'm baby tumeric XOXO photo booth actually, helvetica semiotics austin copper mug. Ascot YOLO crucifix biodiesel DSA occupy. Prism live-edge iPhone semiotics you probably haven't heard of them seitan. Irony shaman 3 wolf moon tacos actually marfa, ascot vegan. Meggings fanny pack before they sold out slow-carb synth literally. Godard chicharrones mustache vegan occupy mumblecore craft beer hella yuccie chartreuse kombucha. Fam bespoke cloud bread actually, stumptown raw denim mukbang hashtag pok pok polaroid banjo master cleanse sartorial. Cray vegan tilde dreamcatcher actually cliche. Waistcoat venmo YOLO, pok pok raclette kombucha locavore williamsburg. Polaroid jianbing yes plz godard sartorial post-ironic shoreditch health goth pinterest. Ugh iceland keffiyeh retro narwhal tumblr",
+  },
+  {
+    id: "6",
+    publishedDate: "05-07-2023",
+    title: "Los Angeles is dope!",
+    author: "Nick Williams",
+    imgUrl:
+      "https://images.unsplash.com/photo-1597982087634-9884f03198ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1488&q=80",
+    text: "I'm baby tumeric XOXO photo booth actually, helvetica semiotics austin copper mug. Ascot YOLO crucifix biodiesel DSA occupy. Prism live-edge iPhone semiotics you probably haven't heard of them seitan. Irony shaman 3 wolf moon tacos actually marfa, ascot vegan. Meggings fanny pack before they sold out slow-carb synth literally. Godard chicharrones mustache vegan occupy mumblecore craft beer hella yuccie chartreuse kombucha. Fam bespoke cloud bread actually, stumptown raw denim mukbang hashtag pok pok polaroid banjo master cleanse sartorial. Cray vegan tilde dreamcatcher actually cliche. Waistcoat venmo YOLO, pok pok raclette kombucha locavore williamsburg. Polaroid jianbing yes plz godard sartorial post-ironic shoreditch health goth pinterest. Ugh iceland keffiyeh retro narwhal tumblr",
+  },
 ];
